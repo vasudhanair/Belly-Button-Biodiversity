32237adff1e6cbd58e87f5d16ed4c3df42526fd9
SSN <vasudhanair@gmail.com>
2020-12-01 14:53:04 -0500

    working on app.js

---
+++ JS/app.js
@@ -0,0 +1,22 @@
+//global variable
+var data;
+
+//init function to fill in the select option
+
+function init() {
+  d3.json("JS/data/samples.json").then(dataInitial => {
+    data = dataInitial;
+    var selectValues = dataInitial.names;
+
+    var selectOpt = d3.select("#selDataset");
+
+    selectValues.forEach(value => {
+      selectOpt
+        .append("option")
+        .text(value)
+        .attr("value", function() {
+          return value;
+        });
+    });
+  });
+}
